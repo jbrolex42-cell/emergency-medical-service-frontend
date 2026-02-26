11e2169bd388bd9ec835a13e866b1787fab54030
jbrolex42-cell <jbrolex42@gmail.com>
2026-02-26 13:35:37 +0300

    updated the api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://emergency-medical-backend.onrender.com',
+  baseURL: 'https://emergency-medical-backend.onrender.com/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
